1f9c22ee4fd8393736a595cac898b4915e493d08
easai <aksarih@gmail.com>
2024-12-31 23:10:49 -0500

    update const

--- fr_verbs/const.py
+++ fr_verbs/const.py
@@ -4,5 +4,6 @@ CONDITIONAL_ETRE = "Condisional être"
 CONDITIONAL_AVOIR = "Condisional avoir"
 PRETERIT_ETRE = "Preterite être"
 PRETERIT_AVOIR = "Preterite avoir"
+IMPERFECT_ETRE = "Imperfect être"
 
 TITLE = "French Verb Quizzes"
